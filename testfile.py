a4a7866d06850c5a7fec14563c6d158260db4b0f
yagizefecsoy <yagizefecsoy@gmail.com>
2026-09-24 13:10:11 +0300

    I made a deliberate mistake to learn 'revert'

--- testfile.py
+++ testfile.py
@@ -4,3 +4,5 @@ print("If not modified, this shall be the last line!")
 print("This is a change.")
 print("This is also a change.")
 
+print("This a mistake to learn how revert works")
+"mistake to learn how revert works"
