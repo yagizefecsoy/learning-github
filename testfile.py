cae85b23d8ed3558743ab2f303ddae96a56a6d34
yagizefecsoy <yagizefecsoy@gmail.com>
2026-09-24 13:13:53 +0300

    Revert "I made a deliberate mistake to learn 'revert'"
    This reverts commit a4a7866d06850c5a7fec14563c6d158260db4b0f.

--- testfile.py
+++ testfile.py
@@ -4,5 +4,3 @@ print("If not modified, this shall be the last line!")
 print("This is a change.")
 print("This is also a change.")
 
-print("This a mistake to learn how revert works")
-"mistake to learn how revert works"
